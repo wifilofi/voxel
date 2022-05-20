f88b8f03217db9d1ba4b46b4d73c1b0eaae005d5
pixel_error <90645177+wifilofi@users.noreply.github.com>
2022-05-20 13:57:33 +0300

    Edit modules Add main.py methods

---
+++ source/main.py
@@ -0,0 +1,17 @@
+
+class Game:
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def update(self):
+        pass
+
+    def render(self):
+        pass
+
+
+game = Game()
+game.start()
