92f4faf80183f13b355c8b81694706afa4a2955d
pixel_error <90645177+wifilofi@users.noreply.github.com>
2022-05-21 03:55:27 +0300

    Edit start height in player.py

--- source/player.py
+++ source/player.py
@@ -7,7 +7,7 @@ class Player:
     def __init__(self):
         self.pos = np.array([0, 0], dtype = float)
         self.angle = math.pi / 4
-        self.height = 100
+        self.height = 500
         self.pitch = 50
         self.angle_velocity = 0.01
         self.velocity = 5
