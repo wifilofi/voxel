3305895186b14b3502a91e7c7efdcf8cc4e53f68
pixel_error <90645177+wifilofi@users.noreply.github.com>
2022-05-21 03:34:00 +0300

    Add player.py controls

--- source/player.py
+++ source/player.py
@@ -1,6 +1,7 @@
 import math
 
 import numpy as np
+import pygame as pg
 
 class Player:
     def __init__(self):
@@ -12,4 +13,40 @@ class Player:
         self.velocity = 5
 
     def update(self):
-        pass
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+
+        pressed_key = pg.key.get_pressed()
+        if pressed_key[pg.K_UP]:
+            self.pitch += self.vel
+
+        if pressed_key[pg.K_DOWN]:
+            self.pitch -= self.vel
+
+        if pressed_key[pg.K_LEFT]:
+            self.angle -= self.angle_vel
+
+        if pressed_key[pg.K_RIGHT]:
+            self.angle += self.angle_vel
+
+        if pressed_key[pg.K_q]:
+            self.height += self.vel
+
+        if pressed_key[pg.K_e]:
+            self.height -= self.vel
+
+        if pressed_key[pg.K_w]:
+            self.pos[0] += self.vel * cos_a
+            self.pos[1] += self.vel * sin_a
+
+        if pressed_key[pg.K_s]:
+            self.pos[0] -= self.vel * cos_a
+            self.pos[1] -= self.vel * sin_a
+
+        if pressed_key[pg.K_a]:
+            self.pos[0] += self.vel * sin_a
+            self.pos[1] -= self.vel * cos_a
+
+        if pressed_key[pg.K_d]:
+            self.pos[0] -= self.vel * sin_a
+            self.pos[1] += self.vel * cos_a
