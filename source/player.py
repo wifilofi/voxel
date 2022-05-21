162c215b0906b07b1f016060a0a03d8a12e11e71
pixel_error <90645177+wifilofi@users.noreply.github.com>
2022-05-21 03:32:29 +0300

    Add player.py fields

--- source/player.py
+++ source/player.py
@@ -1,7 +1,15 @@
+import math
+
+import numpy as np
 
 class Player:
     def __init__(self):
-        pass
+        self.pos = np.array([0, 0], dtype = float)
+        self.angle = math.pi / 4
+        self.height = 100
+        self.pitch = 50
+        self.angle_velocity = 0.01
+        self.velocity = 5
 
     def update(self):
         pass
